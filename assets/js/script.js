0077374f5e1f9319fc5f4af0cb83f8481d46d7d8
Nicolas Dávila <nicolasdavilafsantos@gmail.com>
2023-03-21 20:50:59 -0300

    First Commit

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+const horas = document.getElementById('horas');
+const minutos = document.getElementById('minutos');
+const segundos = document.getElementById('segundos');
+
+const relogio = setInterval(function time() {
+    let dataAtual = new Date();
+    let hr = dataAtual.getHours();
+    let min = dataAtual.getMinutes();
+    let seg = dataAtual.getSeconds();
+
+    if (hr < 10) hr ='0' + hr;
+
+    if (min < 10) min ='0' + min;
+
+    if (seg < 10) seg ='0' + seg;
+
+    horas.textContent = hr;
+    minutos.textContent = min;
+    segundos.textContent = seg;
+
+})
